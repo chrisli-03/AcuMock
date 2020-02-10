9cbf48e911d8e03104a88e69cabcadcdc6cd2f13
HechengLi <lihecheng93@gmail.com>
2020-02-10 17:44:30 -0500

    update

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const path = require('path')
 const fs = require('fs')
 const express = require('express')
 const MockServer = require('./src/MockServer')
-console.log(MockServer)
 
 const app = express()
 const port = 3000
+const mockServers = new Map()
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -16,11 +16,12 @@ fs.readdir(apiDirectory, function(err, files) {
   if (err) {
     return console.log(`Unable to scan directory: ${err}`)
   }
-  const defaultPort = 30326
-  files.forEach(function(file, i) {
+  files.forEach(async function(file, i) {
     if (!/\.json$/.test(file)) return
     const apiData = JSON.parse(fs.readFileSync(path.join(apiDirectory, file)))
-    const mockServer = new MockServer(apiData)
+    const mockServer = new MockServer(apiData.routes, apiData.port)
+    mockServer.start()
+    mockServers.set(file.replace(/\.json$/, ''), mockServer)
   })
 })
 
--- src/MockServer.js
+++ src/MockServer.js
@@ -1,19 +1,23 @@
 const express = require('express')
-const portfinder = require('portfinder')
 
 class MockServer {
-  constructor(apiData) {
-    portfinder.getPort((err, port) => {
-      this.server = express()
-      this.port = port
-      for (const route in apiData) {
-        const api = apiData[route]
-        this.server[api.method](route, function(req, res) {
-          res.status(api.status).send(api.data)
-        })
-      }
-      this.server.listen(this.port, () => { console.log(`Mock server started on port ${this.port}`)})
-    })
+  constructor(apiData, port) {
+    this.app = express()
+    this.port = port
+    for (const route in apiData) {
+      const api = apiData[route]
+      this.app[api.method](route, (req, res) => {
+        res.status(api.status).send(api.data)
+      })
+    }
+  }
+
+  start() {
+    this.server = this.app.listen(this.port, () => { console.log(`Mock server started on port ${this.port}.`)})
+  }
+
+  stop() {
+    this.server.close(() => { console.log(`Mock server on port ${this.port} has stopped.`)})
   }
 }
 
